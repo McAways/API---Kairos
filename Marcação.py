f974f2d5634b0dc3b07fd8101a121957435f8db2
McAways <86322007+McAways@users.noreply.github.com>
2024-08-26 09:33:50 -0300

    Update Marcação.py

--- Marcação.py
+++ Marcação.py
@@ -1,40 +1,44 @@
 import requests
 import pandas as pd
-
+import openpyxl
 
 # Leitor do arquivo excel (mudar o diretório dependendo do pc)
-df = pd.read_excel(r'C:\Users\WI140\Desktop\TESTE JUST\Panda\Marcacao.xlsx')
-# Transforma o campo de data no aceitavel e transformavel para json
-df['data'] = pd.to_datetime(df['data'], format='%d/%m/%Y %H:%M')
+df = pd.read_excel('Envio_de_Marcacao/marcacaoteste.xlsx')
+df['data_hora'] = pd.to_datetime(df['data'].astype(str) + ' ' +
+                                 df['hora'].astype(str),
+                                 format='mixed')
+
+df['data_hora_iso'] = df['data_hora'].dt.strftime('%d/%m/%Y %H:%M')
 
 # Transformando a leitura das colunas em objetos
 for index, row in df.iterrows():
     matricula = row['matricula']
-    data = row['data'].isoformat()
+    data = row['data_hora_iso']
 
-# Cabeça e corpo do arquivo Json
+    # Cabeça e corpo do arquivo Json
+
     headers = {
         "identifier": "90.254.217/0001-10",
         "key": "67a4d6fa-ff21-487c-9426-7e22bb8a9142",
-        "cpf": "32547932008",
         'User-Agent': 'PostmanRuntime/7.30.0'
     }
 
     payload = {
         "Matricula": matricula,
         "DataHoraApontamento": data,
-        "CpfResponsavel": "32547932008",
         "ResponseType": "AS400V1"
     }
-
+    print(data)
     print("Requisição enviada")
     print(payload)
 
-# Requisição Post no endpoint
+    # Requisição Post no endpoint
     response = requests.post(
-        "https://www.dimepkairos.com.br/RestServiceApi/Mark/SetMarks", json=payload, headers=headers)
+        "https://www.dimepkairos.com.br/RestServiceApi/Mark/SetMarks",
+        json=payload,
+        headers=headers)
 
-# Impressão da Resposta
+    # Impressão da Resposta
     print('Resposta da API')
     print(response.text)
 
